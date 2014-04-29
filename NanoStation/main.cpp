37029e863bae0ed5dab16c32c6bfaabac02d3e18
Francois Lorrain <francois.lorrain@gmail.com>
2014-04-29 18:11:57 +0200

    Remove unused files Add PRX Radio code

--- NanoStation/main.cpp
+++ NanoStation/main.cpp
@@ -7,10 +7,6 @@
 
 
 #include "elcheapo_remote.h"
-#include "i2c_keyboard.h"
-#include "nokia5510.h"
-#include "TWI_Master.h"
-#include "adc.h"
 #include "timer0.h"
 
 #include "hal_nrf_reg.h"
@@ -21,9 +17,7 @@
 
 int16_t adc_value;
 uint8_t radio_data[RF_PAYLOAD_LENGTH];
-#ifdef DEBUG
-uint8_t lcd_update;
-#endif
+
 
 
 int main(void) {
@@ -45,156 +39,110 @@ int main(void) {
 	// set CE low (transmit packet when CE pulsed high)
 	CE_LOW();
 
-	lcd_reset();
-	lcd1.begin();
-	TWI_Master_Initialise();
-	adc_init();
-	init_timer0_tick();
-	// enable interrupts
-	__builtin_avr_sei ();
-
-	lcd_update = true;
-
-	lcd1.clear();
-	lcd1.go(0,1);
-	//            123456789012
-	lcd1.print(F("  EL CHEAPO "));
-	lcd1.go(0,2);
-#ifdef RADIO1
-	lcd1.print(F("Remote Ctrl1"));
-#endif
-#ifdef RADIO2
-	lcd1.print(F("Remote Ctrl2"));
-#endif
-	lcd1.update();
-	connected=false;
-	radio_pl_init (NRF_address, HAL_NRF_PTX);
+	lcd = &radio_lcd2;
+//	line = lcd->next_line();
+	hal_nrf_write_lcd_pload(2, 1, lcd->get_next_line(), 13);
 
 	while (1) {
-		lcd1.update();
-#ifdef RADIO1
-		set_loop_time(80/4);
-#endif
-#ifdef RADIO2
-		set_loop_time(90/4);
-#endif
-		kbd1.scan();
-
-		if (!connected) {
-			key = kbd1.get_key();
-			if (key != 0) {
-				lcd1.go(5,5);
-				lcd1.write(key);
-				lcd1.pseudo_led(0,0);
-				if (key == 'A') lcd_update = !lcd_update;
-			} else {
-				lcd1.pseudo_led(0,1);
-			}
-		}
-
-		radio_data[0] = 1; // Normal LCD + ADC packet
-
-		radio_data[1]=kbd1.get_column(0);
-		radio_data[2]=kbd1.get_column(1);
-		radio_data[3]=kbd1.get_column(2);
-		radio_data[4]=kbd1.get_column(3);
-
-		adc_value = get_adc(0);
-		radio_data[5]=adc_value >> 8;
-		radio_data[6]=adc_value & 0xff;
-		if (!connected) {
-			lcd1.go(5,3); lcd1.print(F("     "));lcd1.go(5,3); lcd1.print(adc_value,10);
-		}
-
-		adc_value = get_adc(1);
-		radio_data[7]=adc_value >> 8;
-		radio_data[8]=adc_value & 0xff;
-		if (!connected) {
-			lcd1.go(0,3); lcd1.print(F("     "));lcd1.go(0,3); lcd1.print(adc_value,10);
-		}
-
-		adc_value = get_adc(6);
-		radio_data[9]=adc_value >> 8;
-		radio_data[10]=adc_value & 0xff;
-		if (!connected) {
-			lcd1.go(5,4); lcd1.print(F("     "));lcd1.go(5,4); lcd1.print(adc_value,10);
-		}
-
-		adc_value = get_adc(7);
-		radio_data[11]=adc_value >> 8;
-		radio_data[12]=adc_value & 0xff;
-		if (!connected) {
-			lcd1.go(0,4); lcd1.print(F("     "));lcd1.go(0,4); lcd1.print(adc_value,10);
-		}
-
-		if (lcd_update == 0)
-			lcd1.update();
-
-		hal_nrf_get_clear_irq_flags();
-		radio_send_packet(radio_data, 13);
-		set_timeout(70);
-		lcd1.pseudo_led(9,0);
-		extra_packet=false;
-
-		ready_to_receive:
-		while (!radio_activity()) {
-			if (check_timeout()) break;
-		};
+		CE_HIGH();        // Set Chip Enable (CE) pin high to enable receiver
 		status = hal_nrf_get_status();
-		switch (status & 0x70) {
-		case (1<<HAL_NRF_TX_DS):
-			/* Tx packet sent, ack received but no ack packet payload ... */
-			hal_nrf_get_clear_irq_flags();
-			/* Sent right away a quick packet to try to get the ACK payload but only once*/
-			if (!extra_packet) {
-				extra_packet = true;
-				radio_data[0] = 0;
-				radio_send_packet(radio_data, 1);
-				goto ready_to_receive;
-			}
-			break;
-		case ((1<<HAL_NRF_TX_DS)|(1<<HAL_NRF_RX_DR)):
-			/* Tx done, Ack packet received */
+		if ((status & 0x0e) != 0x0e) { // a packet is available
 			// get it
 			count = hal_nrf_read_reg(R_RX_PL_WID);
-			hal_nrf_read_multibyte_reg(R_RX_PAYLOAD, radio_data, count);
+			hal_nrf_read_multibyte_reg(R_RX_PAYLOAD, pload, count);
 			// clear IRQ source
 			hal_nrf_get_clear_irq_flags();
-			connected = true;
-			lcd1.pseudo_led(9,1);
-			switch (radio_data[0]) {
-			case 0x01: /* LCD data */
-				key = radio_data[1];
-				if ((key > 0) && (key < 6)) {
-					lcd1.go(0,key);
-					for (uint8_t i = 2; i<14; i++)
-						lcd1.write(radio_data[i]);
-				} else {
-					lcd1.go(0,1);
-					lcd1.print(F("Invalid Pkt"));
-				}
+			EIMSK |= (1<<INT7); // Re-enable level sensitive interrupt
+
+			switch ((status & 0x0e) >> 1) {
+			case 1: // Radio1 H/W on channel 1
+				kbd=&radio_kbd1;
+				lcd=&radio_lcd1;
+				/* increment radio_ok counter by 2 */
+				if (kbd->radio_ok != NOT_PRESENT) kbd->radio_ok = NOT_PRESENT;
+				/* decrement other radio count */
+				if (radio_kbd2.radio_ok != 0) radio_kbd2.radio_ok--;
+				ack_pipe = 1;
+#ifdef DEBUG
+				Serial3.write('a');
+#endif
 				break;
-			case 0x02: /* Pseudo_led data */
-				for (i=0; i<8; i++)
-					lcd1.pseudo_led(i, radio_data[i+2]);
+			case 2: // Radio2 H/W on channel 2
+				kbd=&radio_kbd2;
+				lcd=&radio_lcd2;
+				/* increment radio_ok counter by 2 */
+				if (kbd->radio_ok != NOT_PRESENT) kbd->radio_ok = NOT_PRESENT;
+				/* decrement other radio count */
+				if (radio_kbd1.radio_ok != 0) radio_kbd1.radio_ok--;
+				ack_pipe = 2;
+#ifdef DEBUG
+				Serial3.write('b');
+#endif
 				break;
 			default:
-				/* Ignore junk */
+#ifdef DEBUG
+				Serial3.write('M');
+				Serial3.println(status,16);
+#endif
+				continue;
 				break;
 			}
-			break;
-		case (1<<HAL_NRF_MAX_RT):
-			hal_nrf_get_clear_irq_flags();
-			/* Max Retry reached */
-			hal_nrf_flush_tx(); 						// flush tx fifo, avoid fifo jam
-			lcd1.go(0,1);
-			lcd1.print(F("NO ANSWER   "));
-			connected = false;
-			break;
-		default:
-			hal_nrf_get_clear_irq_flags();
-			break;
+			switch (pload[0]) {
+			case 0:
+				/* Just asking for an ack packet cuz sender didn't get one ... */
+				break;
+			case 1:
+				/* Normal kbd/adc packet */
+				kbd->scan_input(pload+1);
+				kbd->pot[0]=(pload[5]<<8)+pload[6];
+				kbd->pot[1]=(pload[7]<<8)+pload[8];
+				kbd->pot[2]=(pload[9]<<8)+pload[10];
+				kbd->pot[3]=(pload[11]<<8)+pload[12];
+				// Now returns led / lines to display
+				if ((status & 0x01) == 0) { // Make sure there is space in TX FIFO
+				line = lcd->next_line();
+				if (line == 0) {
+					// send pseudo LED data
+					hal_nrf_write_led_pload(ack_pipe, lcd->get_pseudo_led(), 11);
+				} else  {
+					hal_nrf_write_lcd_pload(ack_pipe, line , lcd->get_next_line(), 13);
+				}
+				}
+				break;
+			/* Ignore other stuff */
+			default:
+				break;
+			}
+		} else if ((status & (1<<HAL_NRF_MAX_RT)) != 0 ) { // Max Retry, flush TX
+			hal_nrf_flush_tx(); 		// flush tx fifo, avoid fifo jam
+			// TO BE CHECKED .... but does not seem to happen ...
+#ifdef DEBUG
+			Serial3.write('F');
+#endif
+		} else { // Wait for next packet for 2000 ms max
+			if (xSemaphoreTake(radio_done,2000 / portTICK_RATE_MS ) != pdTRUE) {
+				// Timeout on Radio reception
+				radio_kbd1.radio_ok = false;
+				radio_kbd2.radio_ok = false;
+#ifdef DEBUG1
+				Serial3.print(F("ST:"));Serial3.println(hal_nrf_get_status(),16);
+#endif
+				// Reprogram radio
+				CE_LOW();        // Set Chip Enable (CE) pin low during chip init
+				radio_pl_init (HAL_NRF_PRX);
+				// and wait
+#ifdef DEBUG
+				Serial3.write('Z');
+#endif
+			} else {
+				// Signal received from INT7
+#ifdef DEBUG1
+				Serial3.write('i');
+#endif
+			}
 		}
+	} // while
+}
 		/* don't resend a packet too fast */
 		while (!check_loop_time());
 	}
